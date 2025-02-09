ccf282309190fbd7a5fab32c43be2e05e3257d55
rodrigojustinodevs <120591081+rodrigojustinodevs@users.noreply.github.com>
2025-02-09 09:57:09 -0300

    Initial commit

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,72 @@
+<?php return array (
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'nuwave/lighthouse' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Nuwave\\Lighthouse\\LighthouseServiceProvider',
+      1 => 'Nuwave\\Lighthouse\\Async\\AsyncServiceProvider',
+      2 => 'Nuwave\\Lighthouse\\Auth\\AuthServiceProvider',
+      3 => 'Nuwave\\Lighthouse\\Bind\\BindServiceProvider',
+      4 => 'Nuwave\\Lighthouse\\Cache\\CacheServiceProvider',
+      5 => 'Nuwave\\Lighthouse\\GlobalId\\GlobalIdServiceProvider',
+      6 => 'Nuwave\\Lighthouse\\OrderBy\\OrderByServiceProvider',
+      7 => 'Nuwave\\Lighthouse\\Pagination\\PaginationServiceProvider',
+      8 => 'Nuwave\\Lighthouse\\SoftDeletes\\SoftDeletesServiceProvider',
+      9 => 'Nuwave\\Lighthouse\\Testing\\TestingServiceProvider',
+      10 => 'Nuwave\\Lighthouse\\Validation\\ValidationServiceProvider',
+    ),
+  ),
+  'spatie/laravel-ignition' => 
+  array (
+    'aliases' => 
+    array (
+      'Flare' => 'Spatie\\LaravelIgnition\\Facades\\Flare',
+    ),
+    'providers' => 
+    array (
+      0 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
+    ),
+  ),
+);
